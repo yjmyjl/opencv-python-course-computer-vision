df5b77c613feb80f7cb1893c5fe02b72dce098d5
Pratyush <pratyushagarwal101@gmail.com>
2025-03-26 17:39:12 +0530

    Update io_webcam.py

--- 01_io/code/io_webcam.py
+++ 01_io/code/io_webcam.py
@@ -1,16 +1,16 @@
 import cv2
 
 # read webcam
-webcam = cv2.VideoCapture(2)
+webcam = cv2.VideoCapture(0) # 0 denote webcam number
 
 # visualize webcam
 
-while True:
+while True:         # forever
     ret, frame = webcam.read()
 
     cv2.imshow('frame', frame)
-    if cv2.waitKey(40) & 0xFF == ord('q'):
+    if cv2.waitKey(40) & 0xFF == ord('q'): # when pressed "q" window closes
         break
 
-webcam.release()
+webcam.release()     # releases allocated memory
 cv2.destroyAllWindows()
